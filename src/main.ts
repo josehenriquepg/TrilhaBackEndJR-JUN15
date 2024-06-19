5a07effed71a9c285f9bea3c8c01a0493bc7eaad
josehenriquepg <henricopg11@gmail.com>
2024-06-19 15:12:33 -0300

    config: initial configs to start the project

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('To do list challenge')
+    .setDescription('RESTful API for task management with user authentication using Nest.js and SQLite.')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
